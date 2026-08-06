6465d834c00c49387019ba428078790169040716
Priyanshu Gupta <priyanshugupta2569@gmail.com>
2026-08-06 15:48:02 +0530

    feat: add employee CRUD and JWT authentication

---
+++ server/routes/employeeRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getEmployee,
+  createEmployee,
+  deleteEmployee,
+  updateEmployee,
+} from "../controllers/employeeController.js";
+import { protect, protectAdmin } from "../middleware/auth.js";
+
+const employeesRouter = Router();
+
+employeesRouter.get("/", protect, protectAdmin, getEmployee);
+employeesRouter.post("/", protect, protectAdmin, createEmployee);
+employeesRouter.put("/:id", protect, protectAdmin, updateEmployee);
+employeesRouter.delete("/:id", protect, protectAdmin, deleteEmployee);
+export default employeesRouter
