628bbb9e6db71735f73608d476f84e50ce790d22
Priyanshu Gupta <priyanshugupta2569@gmail.com>
2026-08-13 14:16:19 +0530

    frontend configuration

---
+++ client/src/api/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseUrl = (import.meta.env.VITE_BASE_URL || "http://localhost:4000").replace(/\/$/, "");
+
+const api = axios.create({
+  baseURL: `${baseUrl}/api`,
+});
+
+// Attach Auth token to all network requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import {} from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import{BrowserRouter} from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    
   </BrowserRouter>,
-)
+);
